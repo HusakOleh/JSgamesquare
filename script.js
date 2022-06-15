7ca7fa3bcf249d22255be62ffa6e73f5e0234eb0
HusakOleh <husakoleh91@gmail.com>
2022-06-15 07:40:29 +0300

    create new branch

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
-// declare variables
-// asociate with nodes
-// start game
-// end game
 
-const startButton = document.querySelector('#startButton')
-const endButton = document.querySelector('#endButton') //remove
-const gameTime = document.querySelector('#gameTime')
-const gameScore = document.querySelector('#gameScore')
-const clickWrap = document.querySelector('#clickWrap')
+const startButton = document.querySelector('#startButton');
+const endButton = document.querySelector('#endButton');
+const gameTime = document.querySelector('#gameTime');
+const gameScore = document.querySelector('#gameScore');
+const clickWrap = document.querySelector('#clickWrap');
 
+let timeValue = document.querySelector('input');
 
-let timeValue = document.querySelector('input')
-
-gameTime.textContent = ('game time: ' + +timeValue.value)
+gameTime.textContent = ('game time: ' + +timeValue.value);
 
 function setGameTime() {
-    let time = +timeValue.value
-    gameTime.textContent = ('game time: ' + time)
-}
+    let time = +timeValue.value;
+    gameTime.textContent = ('game time: ' + time);
+};
 
 
-timeValue.addEventListener('input', setGameTime)
+timeValue.addEventListener('input', setGameTime);
 
-
-
-
-
-
-startButton.addEventListener('click', startGame)
-
-
-
-
-
-
-
-
-
+startButton.addEventListener('click', startGame);
 
 
 function addMonster() {
     let addDiv = clickWrap.insertAdjacentHTML("afterbegin", '<div id="rS"></div>')
-    randomSquare = document.querySelector('#rS')
-    randomSquare.style.width = randomSquare.style.height = '80px'
-    randomSquare.style.background = 'url("image/monster.png") center/cover'
-    randomSquare.style.position = 'absolute'
-}
+    randomSquare = document.querySelector('#rS');
+    randomSquare.style.width = randomSquare.style.height = '80px';
+    randomSquare.style.background = 'url("image/monster.png") center/cover';
+    randomSquare.style.position = 'absolute';
+};
 
 
 function startGame() {
-    setGameTime()
-    // gameTime.classList.add('hide')
-    // gameScore.classList.remove('hide')
-    startButton.classList.add('hide')
-    console.log(timeValue.value)
-    timeValue.setAttribute('disabled', 'disabled')
+    setGameTime();
+    startButton.classList.add('hide');
+    console.log(timeValue.value);
+    timeValue.setAttribute('disabled', 'disabled');
 
-    addMonster()
+    addMonster();
+};
 
-}
-
+function endGame() {
+    gameTime.classList.add('hide');
+    gameScore.classList.remove('hide');
+};
