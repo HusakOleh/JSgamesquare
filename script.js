9cae1b0d65b16f01b495835098b816afe2c26e5c
HusakOleh <husakoleh91@gmail.com>
2022-06-21 06:52:11 +0300

    finish render monster

--- script.js
+++ script.js
@@ -1,44 +1,86 @@
+const $start = document.querySelector('#startButton');
+const $game = document.querySelector('#clickWrap');
+const $time = document.querySelector('#time');
+const $gameTime = document.querySelector('#gameTime');
+const $score = document.querySelector('#score');
+const $gameScore = document.querySelector('#gameScore');
 
-const startButton = document.querySelector('#startButton');
-const endButton = document.querySelector('#endButton');
-const gameTime = document.querySelector('#gameTime');
-const gameScore = document.querySelector('#gameScore');
-const clickWrap = document.querySelector('#clickWrap');
+let score = 0;
+let isGameStarted = false;
 
-let timeValue = document.querySelector('input');
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleEnemyClick);
 
-gameTime.textContent = ('game time: ' + +timeValue.value);
-
-function setGameTime() {
-    let time = +timeValue.value;
-    gameTime.textContent = ('game time: ' + time);
-};
-
-
-timeValue.addEventListener('input', setGameTime);
-
-startButton.addEventListener('click', startGame);
-
-
-function addMonster() {
-    let addDiv = clickWrap.insertAdjacentHTML("afterbegin", '<div id="rS"></div>')
-    randomSquare = document.querySelector('#rS');
-    randomSquare.style.width = randomSquare.style.height = '80px';
-    randomSquare.style.background = 'url("image/monster.png") center/cover';
-    randomSquare.style.position = 'absolute';
-};
+const hide = $el => $el.classList.add('hide');
+const show = $el => $el.classList.remove('hide');
+const rand = (min, max) => {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+}
 
 
 function startGame() {
-    setGameTime();
-    startButton.classList.add('hide');
-    console.log(timeValue.value);
-    timeValue.setAttribute('disabled', 'disabled');
+    isGameStarted = true;
+    $game.style.background = `url("image/bg${rand(1, 9)}.jpg") center/cover`;
+    hide($start);
 
-    addMonster();
+const interval = setInterval( function () {
+    const time = parseFloat($time.textContent);
+
+    if (time <= 0) {
+        clearInterval(interval);
+        endGame()
+    } else {
+        $time.textContent = (time - 0.1).toFixed(1);
+    };
+
+
+}, 100);
+
+    renderEnemy();
+};
+
+function setGameScore() {
+    $score.textContent = score.toString();
 };
 
 function endGame() {
-    gameTime.classList.add('hide');
-    gameScore.classList.remove('hide');
+    isGameStarted = false;
+    setGameScore();
+    $game.innerHTML = '';
+
+    show($gameScore);
+    hide($gameTime);
+
+    show($start) // перенести в ХТМЛ старт як сіблінг ігрового полотна (не зявляється в дом дереві)
+}
+
+function handleEnemyClick(event) {
+    if (isGameStarted) {
+        if (event.target.dataset.enemy) {
+            score++;
+            renderEnemy();
+        };
+    };
+
+
+
 };
+
+function renderEnemy() {
+    $game.innerHTML = '';
+    const Enemy = document.createElement('div');
+    const enemySize = rand(60, 150);
+    const gameSize = $game.getBoundingClientRect();
+    const maxTop = gameSize.height - enemySize;
+    const maxLeft = gameSize.width - enemySize;
+
+    Enemy.style.position = 'absolute';
+    Enemy.style.width = Enemy.style.height = `${enemySize}px`;
+    Enemy.style.top = `${rand(0, maxTop)}px`;
+    Enemy.style.left = `${rand(0, maxLeft)}px`;
+    Enemy.style.background = `url("image/monster${rand(1, 4)}.png") center/cover`;
+    Enemy.setAttribute('data-enemy', 'true');
+
+
+    $game.insertAdjacentElement('afterbegin', Enemy)
+}
